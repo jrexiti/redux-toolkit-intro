80c2d3e14e6da87dadc007fc4a64c77980cf5662
Jay Rexiti <jrexiti@totalwine.com>
2023-03-01 08:08:13 -0500

    some redux

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  carsReducer,
+  addCar,
+  removeCar,
+  changeSearchTerm,
+} from "./slices/carsSlice";
+import { formSlice, changeName, changeCost } from "./slices/formSlice";
+
+const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+    form: formReducer,
+  },
+});
+
+export { store, changeName, changeCost, addCar, removeCar, changeSearchTerm };
